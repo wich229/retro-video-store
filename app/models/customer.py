d1ee2d6a57bf300cc9786dddadd51edfc974c608
Kyra Patton <kyra.patton@adadevelopersacademy.org>
2022-12-10 13:32:16 -0800

    Add customer model

--- app/models/customer.py
+++ app/models/customer.py
@@ -1,4 +1,11 @@
 from app import db
 
 class Customer(db.Model):
+    __tablename__ = "customer"
+    
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    postal_code = db.Column(db.Integer)
+    phone = db.Column(db.Integer)
+    register_at = db.Column(db.DateTime)
+    
