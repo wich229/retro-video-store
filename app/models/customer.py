328da392492e1eb265b416b21eab4ace8df66f37
Lisa Utsett <lisabethutsett@gmail.com>
2023-01-04 13:56:53 -0500

    Changed attributes in models

--- app/models/customer.py
+++ app/models/customer.py
@@ -2,9 +2,9 @@ from app import db
 
 class Customer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.Text)
-    postal_code = db.Column(db.Varchar(10)) # 10 or 5?
-    phone_number = db.Column(db.Varchar(20)) # ??
+    name = db.Column(db.String)
+    postal_code = db.Column(db.String) 
+    phone_number = db.Column(db.String) 
     register_at = db.Column(db.Datetime)
     videos_checked_out_count = db.Column(db.Integer)
 
--- app/models/video.py
+++ app/models/video.py
@@ -2,6 +2,6 @@ from app import db
 
 class Video(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.Text)
-    release_date = db.Column(db.Datetime)
+    title = db.Column(db.String)
+    release_date = db.Column(db.DateTime, timezone=True)
     total_inventory = db.Column(db.Integer)
