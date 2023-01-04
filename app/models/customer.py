117d079b7899d6d994197d90a0bb51315afc8996
Eva Liu <una229@gmail.com>
2023-01-04 14:08:05 -0500

    add attrs in customer.py and video.py

--- app/models/customer.py
+++ app/models/customer.py
@@ -2,3 +2,26 @@ from app import db
 
 class Customer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    registered_at = db.Column(db.String)
+    postal_code = db.Column(db.String)
+    phone = db.Column(db.String)
+
+
+
+
+
+    #   {
+    #     "id": 1,
+    #     "name": "Shelley Rocha",
+    #     "registered_at": "Wed, 29 Apr 2015 07:54:14 -0700",
+    #     "postal_code": "24309",
+    #     "phone": "(322) 510-8695"
+    #   }
+    
+    def to_dict(self):
+        pass
+    
+    @classmethod
+    def from_dict(cls, data):
+        pass
--- app/models/video.py
+++ app/models/video.py
@@ -2,3 +2,22 @@ from app import db
 
 class Video(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    release_date = db.Column(db.DateTime, timezone=False) # may have to check for timezone
+    total_inventory = db.Column(db.Integer)
+
+
+
+    #   {
+    #     "id": 1,
+    #     "title": "Blacksmith Of The Banished",
+    #     "release_date": "1979-01-18",
+    #     "total_inventory": 10
+    #   }
+    
+    def to_dict(self):
+        pass
+    
+    @classmethod
+    def from_dict(cls, data):
+        pass
